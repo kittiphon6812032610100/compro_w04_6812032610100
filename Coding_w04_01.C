07b494a21e128c170af6ebc75fde2bef347d3107
kittiphon6812032610100 <s6812032610100@email.kmutnb.ac.th>
2025-07-28 11:05:26 +0700

    Add files via upload

---
+++ Coding_w04_01.C
@@ -0,0 +1,6 @@
+#include <stdio.h>  // Keyword
+
+int main() {        // Keyword (int), Identifier (main)
+    printf("Hello,\nC Tokens!"); // Identifier (printf), Constant ("Hello,\nC Tokens!"), Operator (())
+    return 0;        // Keyword (return), Constant (0), Operator (;)
+}
