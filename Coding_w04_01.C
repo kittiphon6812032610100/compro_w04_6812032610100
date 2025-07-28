93c55ca7e37c7ac05e7b3656c780a4e7102a6aab
kittiphon6812032610100 <s6812032610100@email.kmutnb.ac.th>
2025-07-28 11:07:39 +0700

    Delete Coding_w04_01.C

--- Coding_w04_01.C
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>  // Keyword
-
-int main() {        // Keyword (int), Identifier (main)
-    printf("Hello,\nC Tokens!"); // Identifier (printf), Constant ("Hello,\nC Tokens!"), Operator (())
-    return 0;        // Keyword (return), Constant (0), Operator (;)
-}
